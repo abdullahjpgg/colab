d8e93df00fbdec32bd2dfbed29fd8548eca9b098
abdullahjpgg <2.syedabd@gmail.com>
2025-01-14 12:55:25 +0500

    Add files via upload

---
+++ List.py
@@ -0,0 +1,33 @@
+list
+
+# Shopping List
+item_1 : str = "cake"
+item_2 : str = "eggs"
+item_3 : str = "juice"
+item_4 : str = "bread"
+        #   or(in easy terms)
+
+# print(item_1, item_2)
+                        #        0,     1,       2,       3        4
+shopping_items : list[str] = ["cake", "eggs", "juice", "bread", "anda" ]
+# print(shopping_items)
+            #    or
+# print(shopping_items[3])
+
+# print(len(shopping_items))
+
+# print(shopping_items[0:3])
+
+# print("flour" in shopping_items) #output false
+
+# shopping_items.append("anda")
+# print(shopping_items)
+
+# shopping_items.remove("anda")
+# print(shopping_items)
+
+# shopping_items.insert(2, "flour")
+# print(shopping_items)
+
+# shopping_items.pop(4)
+# print(shopping_items)
